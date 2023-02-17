3e7881a1eb4939516264743367818a376e49f809
SadRus <rystixx@yandex.ru>
2023-02-17 15:59:06 +0300

    Change models, edit main.py and runbot.py

--- php_support/models.py
+++ php_support/models.py
@@ -3,21 +3,37 @@ from django.db import models
 
 class Client(models.Model):
     name = models.CharField('Имя', max_length=50)
-    is_access = models.BooleanField('Доступ')
-    chat_id = models.IntegerField('chat_id')
+    is_access = models.BooleanField('Доступ', default=False)
+    user_id = models.IntegerField(
+        'User_id',
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.name
 
 
 class Devman(models.Model):
     name = models.CharField('Имя', max_length=50)
-    is_access = models.BooleanField('Доступ')
-    chat_id = models.IntegerField('chat_id')
+    is_access = models.BooleanField('Доступ', default=False)
+    user_id = models.IntegerField(
+        'User_id',
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.name
 
 
 class Task(models.Model):
     client = models.ForeignKey(
         'Client',
         on_delete=models.CASCADE,
-        verbose_name='Задача',
+        null=True,
+        blank=True,
+        verbose_name='Клиент',
         related_name='tasks',
     )
     devman = models.ForeignKey(
@@ -25,7 +41,7 @@ class Task(models.Model):
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        verbose_name='Задача',
+        verbose_name='Подрядчик',
         related_name='tasks',
     )
     title = models.CharField('Название', max_length=200)
@@ -37,10 +53,16 @@ class Task(models.Model):
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        verbose_name='Задача',
+        verbose_name='Статус',
         related_name='tasks',
     )
+
+    def __str__(self):
+        return self.title
 
 
 class Status(models.Model):
     name = models.CharField('Название', max_length=200)
+
+    def __str__(self):
+        return self.name
