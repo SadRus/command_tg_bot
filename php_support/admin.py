92f4c5478b8762e7345ac1c3b4594de617837a4f
SadRus <rystixx@yandex.ru>
2023-02-19 23:53:12 +0300

    Add some statistic to a django admin and export data to excel

--- php_support/admin.py
+++ php_support/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
+from django.http import HttpResponse
+from django.db.models import Count
 from .models import Client, Devman, Task, Status
+
+from import_export import fields, resources
+from import_export.admin import ImportExportActionModelAdmin, ExportActionMixin, ExportMixin
+from import_export.widgets import ForeignKeyWidget
+from import_export.fields import Field
+
+
+#client
+class ClientResource(resources.ModelResource):
+
+    class Meta:
+        model = Client
 
 
 class ClientTasksInline(admin.TabularInline):
@@ -10,7 +24,30 @@ class ClientTasksInline(admin.TabularInline):
         'status',
     )
     readonly_fields = ('title', )
-    extra = 0
+
+
+class ClientAdmin(ExportMixin, admin.ModelAdmin):
+    search_fields = ('username', 'user_id')
+    readonly_fields = ('username', 'user_id')
+    list_editable = ('is_access', )
+    list_filter = ('is_access', )
+    list_display = [
+        'username',
+        'is_access',
+        'user_id',
+        'tasks_count'
+    ]
+    inlines = [ClientTasksInline]
+    resource_classes = [ClientResource]
+
+    def tasks_count(self, obj):
+        return obj.tasks.count()
+
+#devman
+class DevmanResource(resources.ModelResource):
+
+    class Meta:
+        model = Devman
 
 
 class DevmanTasksInline(admin.TabularInline):
@@ -24,19 +61,7 @@ class DevmanTasksInline(admin.TabularInline):
     extra = 0
 
 
-class ClientAdmin(admin.ModelAdmin):
-    search_fields = ('username', 'user_id')
-    readonly_fields = ('username', 'user_id')
-    list_editable = ('is_access', )
-    list_filter = ('is_access', )
-    list_display = [
-        'username',
-        'is_access',
-        'user_id',
-    ]
-    inlines = [ClientTasksInline]
-
-class DevmanAdmin(admin.ModelAdmin):
+class DevmanAdmin(ExportMixin, admin.ModelAdmin):
     search_fields = ('username', 'user_id')
     readonly_fields = ('username', 'user_id')
     list_editable = ('is_access', )
@@ -47,8 +72,13 @@ class DevmanAdmin(admin.ModelAdmin):
         'user_id',
     ]
     inlines = [DevmanTasksInline]
+    resource_classes = [DevmanResource]
+    
+    def tasks_count(self, obj):
+        return obj.tasks.count()
 
 
+#task
 class TaskAdmin(admin.ModelAdmin):
     search_fields = ('title', )
     readonly_fields = ('client', 'date_start')
